7d861be4e4c829f2d8e40bb56542055a2c730f68
shiva <smpgr2351@gmail.com>
2024-05-26 16:14:00 +0530

    in1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
-import TaskForm from './components/TaskForm';
-import TaskList from './components/TaskList';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
